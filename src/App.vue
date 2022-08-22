efe652f37079bb74ebf1b1bbfda70b5c8ee18c01
tarenadiyaksa <tarenadiyaksa@gmail.com>
2022-08-22 20:28:02 +0700

    Remove: Component

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,9 @@
 <script>
 import Footer from "./components/Footer.vue";
 import Navbar from "./components/Navbar.vue";
-import Coba from "./components/icons/coba.vue";
 
 export default {
   name: "App",
-  components: { Footer, Navbar, Coba },
+  components: { Footer, Navbar },
 };
 </script>
